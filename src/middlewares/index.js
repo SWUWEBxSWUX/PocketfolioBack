659d926c2bcb442d0774c05881714cf2a7f6a439
ella4719 <rhdmstn0424@gmail.com>
2025-02-17 23:59:49 +0900

    Feat : 포트폴리오 모델 보완_

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -23,33 +23,3 @@ exports.verifyToken = (req, res, next) => {
         });
     }
 };
-
-//exports.verifyToken = (req, res, next) => {
-//    try {
-//        // 🔹 "Bearer <TOKEN>" 형식이므로, "Bearer" 제거 후 토큰만 추출
-//        const authHeader = req.headers.authorization;
-//
-//        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//            return res.status(401).json({
-//                code: 401,
-//                message: "토큰이 없습니다.",
-//            });
-//        }
-//
-//        const token = authHeader.split(" ")[1]; // "Bearer" 다음에 오는 실제 토큰만 가져오기
-//        res.locals.decoded = jwt.verify(token, process.env.JWT_SECRET);
-//
-//        return next();
-//    } catch (error) {
-//        if (error.name === "TokenExpiredError") {
-//            return res.status(419).json({
-//                code: 419,
-//                message: "토큰이 만료되었습니다.",
-//            });
-//        }
-//        return res.status(401).json({
-//            code: 401,
-//            message: "유효하지 않은 토큰입니다.",
-//        });
-//    }
-//};
