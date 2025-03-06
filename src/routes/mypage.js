3030393ae4cc0b763e5ddcf7f864fce8a6eef0d2
cinsy26 <siusy2618@naver.com>
2025-03-06 17:39:02 +0900

    FIX: api rui수정

--- src/routes/mypage.js
+++ src/routes/mypage.js
@@ -4,18 +4,21 @@ const router = express.Router();
 const mypageFetchController = require("../controllers/mypage/fetchInfoController");
 const mypageEditController = require("../controllers/mypage/editInfoController");
 
-// 마이페이지 개인정보 가져오기
-router.get("/fetchInfo/:userId", mypageFetchController.fetchMypageInfo);
+const { verifyToken } = require("../middlewares/index"); // ✅ 인증 미들웨어 추가
 
-//마이페이지 포트폴리오 가져오기
-router.get("/portfolio/:userId", mypageFetchController.fetchPortfolioInfo);
-
-// 마이페이지 좋아요 표시한 포트폴리오 가져오기
-router.get("/likes/:userId", mypageFetchController.fetchUserLikedPortfolios);
-
-router.get("/bookmark/:userId", mypageFetchController.fetchUserBookmarks);
-
-//마이페이지 수정
-router.patch("/profile/save", mypageEditController.updateUserProfile);
+// ✅ 모든 요청에 `verifyToken` 미들웨어 적용
+router.get("/fetchInfo", verifyToken, mypageFetchController.fetchMypageInfo);
+router.get("/portfolio", verifyToken, mypageFetchController.fetchPortfolioInfo);
+router.get(
+  "/likes",
+  verifyToken,
+  mypageFetchController.fetchUserLikedPortfolios
+);
+router.get("/bookmark", verifyToken, mypageFetchController.fetchUserBookmarks);
+router.post(
+  "/profile/save",
+  verifyToken,
+  mypageEditController.updateUserProfile
+);
 
 module.exports = router;
