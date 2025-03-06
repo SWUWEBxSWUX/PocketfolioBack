a9d1d396371c86dcd4adf3a491f29d6b3bc47d53
cinsy26 <siusy2618@naver.com>
2025-03-06 17:09:05 +0900

    FIX: mypage router

--- src/routes/mypage.js
+++ src/routes/mypage.js
@@ -1,31 +1,21 @@
 const express = require("express");
+const router = express.Router();
+
 const mypageFetchController = require("../controllers/mypage/fetchInfoController");
 const mypageEditController = require("../controllers/mypage/editInfoController");
 
-const router = express.Router();
-
 // 마이페이지 개인정보 가져오기
-router.get("/mypage/fetchInfo/:userId", mypageFetchController.fetchMypageInfo);
+router.get("/fetchInfo/:userId", mypageFetchController.fetchMypageInfo);
 
-//마이페이지 북마크한 포트폴리오 가져오기
-router.get(
-  "/mypage/portfolio/:userId",
-  verifyToken,
-  mypageFetchController.fetchPortfolioInfo
-);
+//마이페이지 포트폴리오 가져오기
+router.get("/portfolio/:userId", mypageFetchController.fetchPortfolioInfo);
 
 // 마이페이지 좋아요 표시한 포트폴리오 가져오기
-router.get(
-  "/mypage/likes/:userId",
-  verifyToken,
-  mypageFetchController.fetchUserLikedPortfolios
-);
+router.get("/likes/:userId", mypageFetchController.fetchUserLikedPortfolios);
+
+router.get("/bookmark/:userId", mypageFetchController.fetchUserBookmarks);
 
 //마이페이지 수정
-router.patch(
-  "/mypage/profile/save",
-  verifyToken,
-  mypageEditController.updateUserProfile
-);
+router.patch("/profile/save", mypageEditController.updateUserProfile);
 
 module.exports = router;
