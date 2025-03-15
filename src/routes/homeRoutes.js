8abc8beb03b011e14a625652b26ac1a7e9588fea
ella4719 <rhdmstn0424@gmail.com>
2025-03-16 01:36:56 +0900

    Fix : PortfolioLikes -> as: "likes"

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -3,20 +3,11 @@ const homeController = require('../controllers/homeController');
 
 const router = express.Router();
 
-// 추천 포트폴리오 조회
-router.get('/portfolios/recommended', homeController.getRecommendedPortfolios);
-
-// 기존 직군 리스트 조회 → 회사(직군) 조회 API (쿼리 파라미터로 검색어 전달)
-router.get('/jobs/categories', homeController.getJobCategories);
-
 // 포트폴리오 필터링
 router.get('/portfolios/filter', homeController.filterPortfolios);
 
 // 정렬된 포트폴리오 조회
 router.get('/portfolios', homeController.getPortfolios);
-
-// 추천 포트폴리오 변경
-router.post('/portfolios/recommended/update', homeController.updateRecommendedPortfolio);
 
 // 6. 인기 태그 조회
 router.get('/tags/popular', homeController.getPopularTags);
